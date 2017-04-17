2e7b0943eb95ef4b14351057bef4ba677aa85ba1
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-17 14:17:17 -0600

    Test a post for when a link already exists

--- spec/requests/api/v1/links/request_spec.rb
+++ spec/requests/api/v1/links/request_spec.rb
@@ -8,14 +8,19 @@ RSpec.describe 'Post link request', type: :request do
 
     it 'returns JSON of the created link' do
       new_link = { url: "https://www.youtube.com" }.to_json
-
       post "/api/v1/links", new_link
-
       expect(response).to be_success
-
       raw_link = JSON.parse(response.body)
-      
       expect(raw_link.class).to eq(Hash)
+    end
+
+    it 'returns json of an already created link with updated times_read' do
+      expect(@link.times_read).to eq(0)
+      posted_link = { url: 'https://www.twitch.tv' }.to_json
+      post "/api/v1/links", posted_link
+      expect(response).to be_success
+      raw_link = JSON.parse(response.body)
+      expect(raw_link['times_read']).to eq(1)
     end
   end
 end
