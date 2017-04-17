3f1631411cb09e35fc51b6e6442902f12b241f30
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-17 15:13:21 -0600

    Add a test to make sure it works

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::LinksController < ApplicationController
   def create
     link = JSON.parse(request.body.read)
     returned_link = Link.find_or_create_by(url: link['url'])
-    returned_link.times_read += 1
+    num_read = returned_link.times_read += 1
+    returned_link.update_attribute(:times_read, num_read)
+    returned_link.save
     render json: returned_link
   end
 
