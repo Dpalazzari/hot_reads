9ddcded26f39699788cc126b55a50e3ec6bec969
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-17 15:44:27 -0600

    Test model method for top ten links

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe Link, type: :model do
       expect(link).to be_valid
     end
   end
+
+  context '.top_ten' do
+    scenario 'returns top ten links grouped by times_read' do
+      link = create(:link)
+      Link.create(url: "somelink1", times_read: 1)
+      Link.create(url: "somelink2", times_read: 2)
+      Link.create(url: "somelink3", times_read: 3)
+      Link.create(url: "somelink4", times_read: 4)
+      Link.create(url: "somelink5", times_read: 5)
+      Link.create(url: "somelink6", times_read: 6)
+      Link.create(url: "somelink7", times_read: 7)
+      Link.create(url: "somelink8", times_read: 8)
+      Link.create(url: "somelink9", times_read: 9)
+      Link.create(url: "somelink10", times_read: 10)
+
+      links = Link.top_ten
+
+      expect(links.count).to eq(10)
+      
+      expect(links.first.times_read).to be > links[1].times_read
+      expect(links.last.times_read).to be < links[-2].times_read
+    end
+  end
 end
